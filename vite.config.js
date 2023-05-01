00cf15c07a3b6b78ed44d2427046050487d179d7
indranaftena <indranaftena@gmail.com>
2023-05-01 16:07:00 +0700

    add level 3 link

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         'level/try-out': resolve(__dirname, 'level/try-out/index.html'),
         'level/one': resolve(__dirname, 'level/one/index.html'),
         'level/two': resolve(__dirname, 'level/two/index.html'),
+        'level/three': resolve(__dirname, 'level/three/index.html'),
       },
     },
   },
